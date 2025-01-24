d133a8ccb19fd821f296fbb2647b14fba6858467
Darkaleja69 <alejandrogona@gmail.com>
2025-01-24 11:02:17 +0100

    Primeros pasos del proyecto

---
+++ PrimerosPasosProy.py
@@ -0,0 +1,6 @@
+from statsbombpy import sb
+import pandas as pd
+from mplsoccer import VerticalPitch,Pitch
+from highlight_text import ax_text, fig_text
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
